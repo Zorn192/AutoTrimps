73e4ad37fe0a63dfdd4944a6a53698cdb002ea02
Hal Gentz <zegentzy@protonmail.com>
2019-09-01 03:07:18 -0600

    Update .user.js

--- .user.js
+++ .user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         AutoTrimps-Zek
+// @name         AutoTrimps-Zek-Gentz
 // @version      1.0-Zek
-// @namespace    https://ZeGentzy.github.io/AutoTrimps
-// @updateURL    https://ZeGentzy.github.io/AutoTrimps/.user.js
-// @description  Automate all the trimps!
+// @namespace    https://github.com/ZeGentzy/AutoTrimps/raw/fixspires
+// @updateURL    https://github.com/ZeGentzy/AutoTrimps/raw/fixspires/.user.js
+// @description  Automate all the trimps! Gentz
 // @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
@@ -14,9 +14,9 @@
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.id = 'AutoTrimps-Zek';
+script.id = 'AutoTrimps-Zek-Gentz';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://ZeGentzy.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://github.com/ZeGentzy/AutoTrimps/raw/fixspires/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);

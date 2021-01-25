7597072b3db08f6e4c88574821ee829130b1bc75
SadAugust <meandalena@gmail.com>
2021-01-25 17:39:48 +0000

    Update .user.js

--- .user.js
+++ .user.js
@@ -1,22 +1,23 @@
 // ==UserScript==
-// @name         AutoTrimps-SA
-// @version      1.0-SA
-// @namespace    https://SadAugust.github.io/AutoTrimps_Test
-// @updateURL    https://SadAugust.github.io/AutoTrimps_Test/.user.js
+// @name         AutoTrimps-SadAugust
+// @version      1.0-SadAugust
+// @namespace    https://SadAugust.github.io/AutoTrimps
+// @updateURL    https://SadAugust.github.io/AutoTrimps/.user.js
 // @description  Automate all the trimps!
-// @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0
+// @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0, SadAugust
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
-// @connect      *SadAugust.github.io/AutoTrimps_Test*
+// @include      *trimpstest54.netlify.app/*
+// @connect      *SadAugust.github.io/AutoTrimps*
 // @connect      *trimps.github.io*
 // @connect      self
 // @grant        GM_xmlhttpRequest 
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.id = 'AutoTrimps-SA';
+script.id = 'AutoTrimps-SadAugust';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://SadAugust.github.io/AutoTrimps_Test/AutoTrimps2.js';
+script.src = 'https://SadAugust.github.io/AutoTrimps/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);

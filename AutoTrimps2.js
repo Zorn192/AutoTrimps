2cefd0f1a6e1ad6ecdfa26ac4dff8f91b7c61780
asn <gitea@asinger.de>
2026-08-25 13:16:00 +0200

    Added 'Mutated' heirlooms

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -1,4 +1,4 @@
-var ATversion = 'Zek v5.1.0',
+var ATversion = 'Zek v5.3.0',
     atscript = document.getElementById('AutoTrimps-script'),
     basepath = 'https://Zorn192.github.io/AutoTrimps/', //Link to your own Github here if you forked!
     modulepath = 'modules/';
@@ -34,10 +34,16 @@ function initializeAutoTrimps() {
 
 var changelogList = [];
 changelogList.push({
+    date: "25/08/2026",
+    version: "v5.3.0",
+    description: "<b>Trimps v5.10.1</b> Added Mutated Heirlooms. ",
+    isNew: true
+});
+changelogList.push({
     date: "11/02/2023",
     version: "v5.2.0",
     description: "<b>Trimps v5.9.0</b> Added Frigid to calc. Added Desolation AutoDeso. Added mutations to calc. ",
-    isNew: true
+    isNew: false
 });
 changelogList.push({
     date: "13/11/2022",
